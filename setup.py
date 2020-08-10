d3cc0feb33865bb7be44d1cb23931629b652c8f7
matllubos <matllubos@gmail.com>
2020-08-10 17:01:58 +0200

    0.0.4

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-user-comments',
-    version='0.0.3',
+    version='0.0.4',
     description='Library for adding comments of users related with object',
     author='Lubos Matl',
     author_email='matllubos@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'django-chamber~=0.4.6',
+        'django-chamber>=0.5.25',
     ],
     include_package_data=True,
     zip_safe=False,
